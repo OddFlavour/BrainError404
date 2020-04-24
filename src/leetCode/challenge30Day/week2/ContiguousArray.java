2758309245e10e35c331ec5341817d2a03caa286
OddFlavour <geezwtfaddmeftw@hotmail.com>
2020-04-23 22:06:05 -0400

    - LeetCode|30DayChallenge: 1 solution added

--- src/leetCode/challenge30Day/week2/ContiguousArray.java
+++ src/leetCode/challenge30Day/week2/ContiguousArray.java
@@ -1,50 +1,64 @@
 package leetCode.challenge30Day.week2;
 
+import java.util.*;
+
 public class ContiguousArray {
-
     public int findMaxLength(int[] nums) {
-        int ans = 0;
-        // loop through each possible start
-        for (int i = 0; i < nums.length; i++) {
-            int ones = 0;
-            int zeroes = 0;
-            // from possible start, loop through list
-            for (int j = i; j < nums.length; j++) {
-                if (nums[j] == 1) {
-                    ones++;
-                } else {
-                    zeroes++;
-                }
+        int[] sums = new int[nums.length + 1];
+        sums[0] = 0;
 
-                if (ones == zeroes) {
-                    if (ones + zeroes > ans) {
-                        ans = ones + zeroes;
-                    }
+        int curr = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 1) {
+                curr--;
+            } else {
+                curr++;
+            }
+
+            sums[i + 1] = curr;
+        }
+
+        int ans = 0;
+
+        Map<Integer, Integer> starts = new HashMap<>();
+        for (int i = 0; i < sums.length; i++) {
+            Integer start = starts.get(sums[i]);
+            if (start == null) {
+                starts.put(sums[i], i);
+            } else {
+                int possibleAns = i - start;
+                if (possibleAns > ans) {
+                    ans = possibleAns;
                 }
             }
         }
 
         return ans;
     }
-
     public static void main(String[] args) {
-        int[] t1 = { 0,1 };
-        int[] t2 = { 0,1,0 };
-        int[] t3 = {
-                0,0,0,0,0,0,0,0,0,0,
-                1,1,1,1,1,1,1,1,1
-        };
-        int[] t4 = { 0 };
-        int[] t5 = { };
-        int[] t6 = { 0, 1, 0, 0, 1, 0, 0, 1 };
+        List<List<Integer>> tests = new ArrayList<>();
+        int[] expected = {2, 2, 18, 0, 0, 4, 4, 68, 6, 6};
 
-        ContiguousArray s = new ContiguousArray();
-        assertTrue(s.findMaxLength(t1), 2);
-        assertTrue(s.findMaxLength(t2), 2);
-        assertTrue(s.findMaxLength(t3), 18);
-        assertTrue(s.findMaxLength(t4), 0);
-        assertTrue(s.findMaxLength(t5), 0);
-        assertTrue(s.findMaxLength(t6), 4);
+        tests.add(new ArrayList<>(Arrays.asList(0, 1)));
+        tests.add(new ArrayList<>(Arrays.asList(0, 1, 0)));
+        tests.add(new ArrayList<>(Arrays.asList(
+                0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                1, 1, 1, 1, 1, 1, 1, 1, 1
+        )));
+        tests.add(new ArrayList<>(Arrays.asList(0)));
+        tests.add(new ArrayList<>());
+        tests.add(new ArrayList<>(Arrays.asList(0, 1, 0, 0, 1, 0, 0, 1)));
+        tests.add(new ArrayList<>(Arrays.asList(0, 1, 1, 0, 1, 1, 1, 0)));
+        tests.add(new ArrayList<>(Arrays.asList(0, 1, 0, 1, 1, 1, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 0, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1)));
+        tests.add(new ArrayList<>(Arrays.asList(1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1)));
+        tests.add(new ArrayList<>(Arrays.asList(1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 1)));
+
+        for (int i = 0; i < tests.size(); i++) {
+            ContiguousArray s = new ContiguousArray();
+            int[] test = tests.get(i).stream().mapToInt(t -> t).toArray();
+
+            assertTrue(s.findMaxLength(test), expected[i]);
+        }
     }
 
     public static void assertTrue(int a, int b) {
