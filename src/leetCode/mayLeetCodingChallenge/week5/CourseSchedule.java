4301ef8d4293970bea71a0877ff4d31775d98f59
oddflavour <kavkeon@gmail.com>
2020-06-09 19:35:43 -0400

    Revised 1 solution

--- src/leetCode/mayLeetCodingChallenge/week5/CourseSchedule.java
+++ src/leetCode/mayLeetCodingChallenge/week5/CourseSchedule.java
@@ -3,19 +3,25 @@ package leetCode.mayLeetCodingChallenge.week5;
 import java.util.*;
 
 public class CourseSchedule {
-    class Graph {
-        boolean[] visited;
-        Map<Integer, Set<Integer>> paths;
+    int N;
 
-        public Graph(int N) {
-            visited = new boolean[N];
-            paths = new HashMap<>();
-        }
-    }
-
+    /**
+     * Runs in about 10ms on LC
+     * @param numCourses
+     * @param prerequisites
+     * @return
+     */
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        Graph g = new Graph(numCourses);
-        Graph gT = new Graph(numCourses);
+        N = numCourses;
+
+        boolean[] visited = new boolean[N];
+
+        Map<Integer, Set<Integer>> paths = new HashMap<>();
+
+        // Transpose
+        boolean[] visitedT = new boolean[N];
+
+        Map<Integer, Set<Integer>> pathsT = new HashMap<>();
 
         // Build paths
         for (int[] path : prerequisites) {
@@ -23,11 +29,11 @@ public class CourseSchedule {
 
             if (a == b) return false;
 
-            if (!g.paths.containsKey(a)) g.paths.put(a, new HashSet<>());
-            g.paths.get(a).add(b);
+            if (!paths.containsKey(a)) paths.put(a, new HashSet<>());
+            paths.get(a).add(b);
 
-            if (!gT.paths.containsKey(b)) gT.paths.put(b, new HashSet<>());
-            gT.paths.get(b).add(a);
+            if (!pathsT.containsKey(b)) pathsT.put(b, new HashSet<>());
+            pathsT.get(b).add(a);
         }
 
         /*
@@ -37,14 +43,14 @@ public class CourseSchedule {
         // Find strongly connected components
         Stack<Integer> s = new Stack<>();
         for (int i = 0; i < numCourses; i++) {
-            dfs(s, g, i);
+            dfs(s, paths, visited, i);
         }
 
         while (!s.isEmpty()) {
             int focus = s.pop();
 
             Stack<Integer> scc = new Stack<>();
-            dfs(scc, gT, focus);
+            dfs(scc, pathsT, visitedT, focus);
 
             if (scc.size() >= 2) return false;
         }
@@ -52,18 +58,72 @@ public class CourseSchedule {
         return true;
     }
 
-    private void dfs(Stack<Integer> s, Graph g, int start) {
-        if (g.visited[start]) return;
+    private void dfs(Stack<Integer> s, Map<Integer, Set<Integer>> paths, boolean[] visited, int start) {
+        if (visited[start]) return;
 
-        g.visited[start] = true;
+        visited[start] = true;
 
-        if (g.paths.containsKey(start)) {
-            for (int end : g.paths.get(start)) {
-                dfs(s, g, end);
+        if (paths.containsKey(start)) {
+            for (int i : paths.get(start)) {
+                dfs(s, paths, visited, i);
             }
         }
 
         s.add(start);
+    }
+
+    /**
+     * Runs in about 5ms on LC
+     * @param numCourses
+     * @param prerequisites
+     * @return
+     */
+    public boolean canFinish2(int numCourses, int[][] prerequisites) {
+        N = numCourses;
+
+        boolean[] visited = new boolean[N];
+        boolean[] recStack = new boolean[N];
+
+        Map<Integer, Set<Integer>> paths = new HashMap<>();
+
+        // Build paths
+        for (int[] path : prerequisites) {
+            int a = path[0], b = path[1];
+
+            if (a == b) return false;
+
+            if (!paths.containsKey(a)) paths.put(a, new HashSet<>());
+            paths.get(a).add(b);
+        }
+
+        for (int i = 0; i < numCourses; i++) {
+            if (!dfs2(paths, visited, recStack, i)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private boolean dfs2(Map<Integer, Set<Integer>> paths, boolean[] visited, boolean[] recStack, int start) {
+        if (recStack[start]) return false;
+
+        if (visited[start]) return true;
+
+        visited[start] = true;
+        recStack[start] = true;
+
+        if (paths.containsKey(start)) {
+            for (int i : paths.get(start)) {
+                if (!dfs2(paths, visited, recStack, i)) {
+                    return false;
+                }
+            }
+        }
+
+        recStack[start] = false;
+
+        return true;
     }
 
     public static void main(String[] args) {
@@ -87,13 +147,10 @@ public class CourseSchedule {
 
         for (int i = 0; i < testNs.length; i++) {
             CourseSchedule s = new CourseSchedule();
-            System.out.println(s.canFinish(testNs[i], testPrereqs[i]));
-
-            long start = System.currentTimeMillis();
-            s.canFinish(testNs[i], testPrereqs[i]);
-            long end = System.currentTimeMillis();
-
-            System.out.println(end - start);
+            System.out.println(
+                    s.canFinish(testNs[i], testPrereqs[i])
+                            == s.canFinish2(testNs[i], testPrereqs[i])
+            );
         }
     }
 }
